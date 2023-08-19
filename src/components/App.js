63ebf2d057e7bf617daa57d5ff7967e5fe847394
scobett <kevin@Scobett>
2023-08-19 17:39:49 +0300

    Complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
-import React from "react";
-import ColorList from "./ColorList";
+import React from 'react';
+import NavBar from './NavBar';
+import ProjectList from './ProjectList';
 
-function App() {
+const App = () => {
+  const links = ['Home', 'About', 'Contact'];
+  const projects = [
+    {
+      id: 1,
+      name: 'Project 1',
+      description: 'Description for Project 1',
+    },
+    {
+      id: 2,
+      name: 'Project 2',
+      description: 'Description for Project 2',
+    },
+  ];
+
+  const technologies = ['React', 'Node.js', 'Express.js'];
+
   return (
     <div>
-      <ColorList />
+      <NavBar links={links} />
+      <ProjectList projects={projects} />
     </div>
   );
-}
+};
 
 export default App;
